361ffad1180f7cab71e7429fb2090fac0d515594
Bogdan Popa <bogdan@defn.io>
2019-06-02 15:35:22 +0300

    util: add call-with-timeout

--- koyo-lib/koyo/util.rkt
+++ koyo-lib/koyo/util.rkt
@@ -2,6 +2,8 @@
 
 (require racket/contract)
 
+;; Boxes ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (provide
  box-swap!)
 
@@ -10,3 +12,29 @@
   (let loop ([v (unbox b)])
     (unless (box-cas! b v (f v))
       (loop (unbox b)))))
+
+
+;; Threads ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(provide
+ call-with-timeout)
+
+(define/contract (call-with-timeout f #:timeout [timeout 5000])
+  (->* ((-> any/c))
+       (#:timeout exact-nonnegative-integer?)
+       (or/c 'timeout any/c))
+
+  (define deadline (+ (current-inexact-milliseconds) timeout))
+  (define chan (make-channel))
+  (define worker
+    (thread
+     (lambda ()
+       (channel-put chan (f)))))
+
+  (sync/enable-break
+   (choice-evt chan
+               (wrap-evt
+                (alarm-evt deadline)
+                (lambda _
+                  (begin0 'timeout
+                    (kill-thread worker)))))))
