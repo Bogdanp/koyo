eb3ccf5c5da04e3afd12453b6fce92a63f60fa82
Bogdan Popa <bogdan@defn.io>
2019-06-02 23:05:10 +0300

    build: add missing deps to the test package

--- koyo-test/info.rkt
+++ koyo-test/info.rkt
@@ -4,7 +4,11 @@
 
 (define deps '())
 (define build-deps '("base"
+                     "component-lib"
+                     "db-lib"
+                     "gregor-lib"
                      "koyo-lib"
-                     "rackunit-lib"))
+                     "rackunit-lib"
+                     "web-server-lib"))
 
 (define update-implies '("koyo-lib"))
