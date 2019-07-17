c9165fd507f4bb05773243935ffd36402813d8b3
Bogdan Popa <bogdan@defn.io>
2019-07-17 15:21:46 +0300

    mail: add mailer-send-password-reset-email

--- koyo-lib/blueprints/standard/app-name-here/components/mail.rkt
+++ koyo-lib/blueprints/standard/app-name-here/components/mail.rkt
@@ -3,11 +3,13 @@
 (require koyo/mail
          koyo/url
          racket/contract
+         racket/string
          "user.rkt")
 
 (provide
  (all-from-out koyo/mail)
- mailer-send-welcome-email)
+ mailer-send-welcome-email
+ mailer-send-password-reset-email)
 
 (define/contract (mailer-send-welcome-email m user)
   (-> mailer? user? void?)
@@ -24,3 +26,23 @@
                       'action_url action-url
                       'name (user-username user)
                       'username (user-username user))))
+
+(define/contract (mailer-send-password-reset-email m user token)
+  (-> mailer? user? non-empty-string? void?)
+
+  (define action-url
+    (make-application-url "password-reset" (number->string (user-id user)) token))
+
+  (mail-adapter-send-email-with-template
+   (mailer-adapter m)
+   #:to (user-username user)
+   #:from (mailer-sender m)
+   #:template-alias "password-reset"
+   #:template-model (mailer-merge-common-variables m
+                      'action_url action-url
+                      'name (user-username user)
+                      'username (user-username user))))
+
+;; Local Variables:
+;; eval: (put 'mailer-merge-common-variables 'racket-indent-function #'defun)
+;; End:
