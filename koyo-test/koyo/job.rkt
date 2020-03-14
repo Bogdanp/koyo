ffecac0c9642b95faf73e5a0c555251cbd8b3251
Bogdan Popa <bogdan@defn.io>
2020-03-14 22:17:46 +0200

    tests: test a job that uses the current-system

--- koyo-test/koyo/job.rkt
+++ koyo-test/koyo/job.rkt
@@ -8,6 +8,7 @@
          koyo/job/broker
          koyo/job/serialize
          racket/async-channel
+         racket/class
          racket/match
          rackunit)
 
@@ -18,69 +19,98 @@
 (define-job (add x y)
   (+ x y))
 
+(define-job (publish message)
+  (define db (system-ref (current-system) 'database))
+  (with-database-connection [conn db]
+    (query-exec conn "SELECT PG_NOTIFY('messages', $1)" message)))
+
+(define messages
+  (make-async-channel))
+
 (define-system worker
   [broker (database) make-broker]
-  [database (make-database-factory (lambda _
-                                     (postgresql-connect #:database "koyo"
-                                                         #:user     "koyo"
-                                                         #:password "koyo")))])
+  [database (make-database-factory
+             (lambda ()
+               (postgresql-connect #:database "koyo"
+                                   #:user     "koyo"
+                                   #:password "koyo"
+                                   #:notification-handler
+                                   (lambda (channel message)
+                                     (when (string=? channel "messages")
+                                       (async-channel-put messages message))))))])
+
+(define (await-next-message db)
+  (thread
+   (lambda ()
+     (with-database-connection [conn db]
+       (query-exec conn "LISTEN messages")
+       (let loop ()
+         (sync
+          (handle-evt
+           (send+ conn
+                  (get-base)
+                  (async-message-evt))
+           (lambda (ready?)
+             (unless ready?
+               (loop))))))))))
 
 (define job-tests
-  (let ([stop-worker #f])
-    (test-suite
-     "job"
+  (test-suite
+   "job"
 
-     (system-test-suite job
-       ([broker (database) make-broker]
-        [database (make-database-factory (lambda _
-                                           (postgresql-connect #:database "koyo"
-                                                               #:user     "koyo"
-                                                               #:password "koyo")))])
+   (system-test-suite job
+     ([broker (database) make-broker]
+      [database (make-database-factory (lambda ()
+                                         (postgresql-connect #:database "koyo"
+                                                             #:user     "koyo"
+                                                             #:password "koyo")))])
 
-       #:before
-       (lambda _
-         (with-database-connection [conn database]
-           (query-exec conn "truncate table koyo_jobs")))
+     #:before
+     (lambda _
+       (with-database-connection [conn database]
+         (query-exec conn "truncate table koyo_jobs")))
 
-       (test-case "jobs can be executed synchronously"
-         (parameterize ([execute-jobs-synchronously? #t])
-           (check-false (add 1 2))))
+     (test-case "jobs can be executed synchronously"
+       (parameterize ([execute-jobs-synchronously? #t])
+         (check-false (add 1 2))))
 
-       (test-case "jobs can be {en,de}queued"
-         (parameterize ([current-broker broker])
-           (check-not-false (add 1 2)))
+     (test-case "jobs can be {en,de}queued"
+       (parameterize ([current-broker broker])
+         (check-not-false (add 1 2)))
 
-         (match-define (list (vector id queue job arguments attempts))
-           (broker-dequeue! broker 0 "default"))
+       (match-define (list (vector id queue job arguments attempts))
+         (broker-dequeue! broker 0 "default"))
 
-         (check-equal?
-          (list null null '(1 2))
-          (deserialize arguments))
+       (check-equal?
+        (list null null '(1 2))
+        (deserialize arguments))
 
-         (broker-mark-done! broker id)
-         (check-equal? (broker-dequeue! broker 0 "default") null)))
+       (broker-mark-done! broker id)
+       (check-equal? (broker-dequeue! broker 0 "default") null)))
 
-     (let ([broker #f]
-           [database #f]
-           [stop-worker #f])
-       (test-suite
-        "worker"
+   (let ([broker #f]
+         [database #f]
+         [stop-worker #f])
+     (test-suite
+      "worker"
 
-        #:before
-        (lambda _
-          (set! stop-worker (start-worker worker-system))
-          (set! broker (system-ref worker-system 'broker))
-          (set! database (system-ref worker-system 'database))
-          (with-database-connection [conn database]
-            (query-exec conn "truncate table koyo_jobs")))
+      #:before
+      (lambda _
+        (set! stop-worker (start-worker worker-system))
+        (set! broker (system-ref worker-system 'broker))
+        (set! database (system-ref worker-system 'database))
+        (with-database-connection [conn database]
+          (query-exec conn "truncate table koyo_jobs")))
 
-        #:after
-        (lambda _
-          (stop-worker))
+      #:after
+      (lambda _
+        (stop-worker))
 
-        (test-case "jobs can be executed"
-          (parameterize ([current-broker broker])
-            (add 1 2))))))))
+      (test-case "jobs can be executed"
+        (parameterize ([current-broker broker])
+          (await-next-message database)
+          (publish "hello")
+          (check-equal? (sync messages) "hello")))))))
 
 (module+ test
   (require rackunit/text-ui)
