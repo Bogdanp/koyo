5183cef17fc390944404db4827853ddff782ee40
Bogdan Popa <bogdan@defn.io>
2020-03-29 13:31:56 +0300

    server: accept #:limits and wait on the confirmations channel

--- koyo-doc/scribblings/server.scrbl
+++ koyo-doc/scribblings/server.scrbl
@@ -5,7 +5,9 @@
                      racket/contract
                      racket/string
                      web-server/dispatchers/dispatch
-                     web-server/http)
+                     web-server/http
+                     web-server/safety-limits
+                     web-server/web-server)
           "koyo.rkt")
 
 @title[#:tag "server"]{Server}
@@ -19,7 +21,10 @@ This module provides a web server component.
 }
 
 @defproc[((make-server-factory [#:host host non-empty-string? "127.0.0.1"]
-                               [#:port port (integer-in 0 65535) 8000]) [dispatcher dispatcher/c]) server?]{
+                               [#:port port (integer-in 0 65535) 8000]
+                               [#:limits limits safety-limits? (make-safety-limits)]) [dispatcher dispatcher/c]) server?]{
   Creates a component that, when started, runs a web server bound to
   @racket[host] and @racket[port] using the given @racket[dispatcher].
+
+  The @racket[limits] are passed directly to @racket[serve].
 }
--- koyo-lib/koyo/server.rkt
+++ koyo-lib/koyo/server.rkt
@@ -1,9 +1,11 @@
 #lang racket/base
 
 (require component
+         racket/async-channel
          racket/contract
          racket/string
          web-server/dispatchers/dispatch
+         web-server/safety-limits
          web-server/web-server)
 
 (provide
@@ -12,14 +14,21 @@
 
 (define-logger server)
 
-(struct server (host port dispatcher stopper)
+(struct server (host port limits dispatcher stopper)
   #:transparent
   #:methods gen:component
   [(define (component-start a-server)
+     (define ch (make-async-channel))
      (define stopper
        (serve #:dispatch (server-dispatcher a-server)
               #:listen-ip (server-host a-server)
-              #:port (server-port a-server)))
+              #:port (server-port a-server)
+              #:safety-limits (server-limits a-server)
+              #:confirmation-channel ch))
+
+     (define maybe-exn (sync ch))
+     (when (exn:fail? maybe-exn)
+       (raise maybe-exn))
 
      (log-server-info "listening on ~a:~a"
                       (server-host a-server)
@@ -31,9 +40,11 @@
      (struct-copy server a-server [stopper #f]))])
 
 (define/contract ((make-server-factory #:host [host "127.0.0.1"]
-                                       #:port [port 8000]) dispatcher)
+                                       #:port [port 8000]
+                                       #:limits [limits (make-safety-limits)]) dispatcher)
   (->* ()
        (#:host non-empty-string?
-        #:port (integer-in 0 65535))
+        #:port (integer-in 0 65535)
+        #:limits safety-limits?)
        (-> dispatcher/c server?))
-  (server host port dispatcher #f))
+  (server host port limits dispatcher #f))
