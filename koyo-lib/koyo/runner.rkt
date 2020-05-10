a61266252a9c5447454c1bae7ab13eca8dbec842
Bogdan Popa <bogdan@defn.io>
2020-05-10 16:19:41 +0300

    runner: simplify filesystem change watching

--- koyo-lib/koyo/runner.rkt
+++ koyo-lib/koyo/runner.rkt
@@ -15,6 +15,11 @@
 (define-logger runner)
 (define-logger watcher)
 
+(define racket-exe (find-executable-path "racket"))
+(define raco-exe (find-executable-path "raco"))
+(define (raco . args)
+  (apply system*/exit-code raco-exe args))
+
 (define (track-file? p)
   (if (directory-exists? p)
       (match/values (split-path p)
@@ -28,42 +33,20 @@
         [(#".html" #".sql")        #t]
         [else                      #f])))
 
-(define (track path handler)
-  (define (collect-tracked-files)
-    (map simplify-path (find-files track-file? path #:skip-filtered-directory? #t)))
+(define (collect-tracked-files path)
+  (map simplify-path (find-files track-file? path #:skip-filtered-directory? #t)))
 
-  (define parent-custodian
-    (current-custodian))
-
-  (let loop ([tracked-files (collect-tracked-files)])
-    (parameterize ([current-custodian (make-custodian parent-custodian)])
-      (define new-tracked-files
-        (sync
-         (handle-evt
-          (filesystem-change-evt path)
-          (lambda _
-            (collect-tracked-files)))
-
-         (handle-evt
-          (apply choice-evt (filter-map
-                             (lambda (p)
-                               (and (file-exists? p)
-                                    (handle-evt (filesystem-change-evt p) (const p))))
-                             tracked-files))
-          (lambda (p)
-            (begin0 tracked-files
-              (handler p))))))
-
-      (custodian-shutdown-all (current-custodian))
-      (loop new-tracked-files))))
-
-(define (watch path handler)
-  (thread
-   (lambda ()
-     (log-watcher-debug "starting watcher for path ~v" (path->string path))
-     (track path (lambda (changed-path)
-                   (log-watcher-debug "detected change in ~v" (path->string changed-path))
-                   (handler changed-path))))))
+(define (code-change-evt root-path)
+  (define custodian (make-custodian))
+  (parameterize ([current-custodian custodian])
+    (apply
+     choice-evt
+     (for/list ([p (in-list (collect-tracked-files root-path))] #:when (file-exists? p))
+       (handle-evt
+        (filesystem-change-evt p)
+        (lambda _
+          (begin0 p
+            (custodian-shutdown-all custodian))))))))
 
 (define/contract (run-forever dynamic-module-path
                               #:recompile? [recompile? #t]
@@ -75,13 +58,12 @@
   (file-stream-buffer-mode (current-output-port) 'line)
   (file-stream-buffer-mode (current-error-port) 'line)
 
+  (define root-path (normalize-path (build-path dynamic-module-path 'up 'up)))
   (define command-args
     (if errortrace?
         (list "-l" "errortrace" "-t" dynamic-module-path)
         (list dynamic-module-path)))
 
-  (define stop #f)
-  (define rb (make-barrier))
   (define (run)
     (match-define (list in out pid err control)
       (parameterize ([subprocess-group-enabled #t])
@@ -89,11 +71,10 @@
                (current-output-port)
                (current-input-port)
                (current-error-port)
-               (find-executable-path "racket")
+               racket-exe
                command-args)))
 
     (log-runner-info "application process started with pid ~a" pid)
-    (barrier-close rb)
 
     (values
      (handle-evt
@@ -104,61 +85,38 @@
         (control 'status)))
      (lambda ()
        (control 'interrupt)
-       (control 'wait)
-       (barrier-open rb))))
+       (control 'wait))))
 
   (define (make!)
-    (system*/exit-code
-     (find-executable-path "raco")
-     "make"
-     dynamic-module-path))
+    (raco "make" dynamic-module-path))
 
   (when recompile?
     (log-runner-info "compiling application")
     (make!))
 
   (log-runner-info "starting application process")
-  (watch (simplify-path (build-path dynamic-module-path 'up 'up))
-         (lambda (changed-path)
-           (when recompile?
-             (log-runner-info (format "recompiling because '~a' changed" changed-path))
-             (make!))
-           (when stop
-             (log-runner-info "stopping application process")
-             (stop))))
-
   (let loop ()
-    (define-values (stopped-evt stop-process)
-      (run))
+    (let-values ([(stopped-evt stop) (run)])
+      (with-handlers ([exn:break?
+                       (lambda _
+                         (stop))])
+        (sync/enable-break
+         (handle-evt
+          stopped-evt
+          (lambda (status)
+            (when (eq? status 'done-error)
+              (log-runner-warning "application process failed; waiting for changes before reloading")
+              (sync (code-change-evt root-path)))
 
-    (set! stop stop-process)
-    (with-handlers ([exn:break?
-                     (lambda (_)
-                       (stop))])
-      (sync/enable-break
-       (handle-evt
-        stopped-evt
-        (lambda (status)
-          (when (eq? status 'done-error)
-            (log-runner-warning "application process failed; waiting for changes before reloading")
-            (barrier-wait rb))
-
-          (log-runner-info "restarting application process")
-          (loop)))))))
-
-
-;; barrier ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(define (make-barrier)
-  (make-semaphore 0))
-
-(define (barrier-open b)
-  (semaphore-post b))
-
-(define (barrier-wait b)
-  (semaphore-wait b))
-
-(define (barrier-close b)
-  (let loop ()
-    (when (semaphore-try-wait? b)
-      (loop))))
+            (log-runner-info "restarting application process")
+            (loop)))
+         (handle-evt
+          (code-change-evt root-path)
+          (lambda (changed-path)
+            (when recompile?
+              (log-runner-info (format "recompiling because '~a' changed" changed-path))
+              (make!))
+            (when stop
+              (log-runner-info "stopping application process")
+              (stop))
+            (loop))))))))
