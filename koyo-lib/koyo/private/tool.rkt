14563dec533e2c603e0156b8c3eb4a1087db1fd7
Bogdan Popa <bogdan@defn.io>
2021-07-30 11:24:41 +0300

    tool: disable parallel compilation for racket <8.0.2.4

--- koyo-lib/koyo/private/tool.rkt
+++ koyo-lib/koyo/private/tool.rkt
@@ -2,7 +2,8 @@
 
 (require racket/format
          racket/future
-         racket/system)
+         racket/system
+         version/utils)
 
 (provide
  racket-exe
@@ -10,13 +11,19 @@
  raco
  make!)
 
+;; Before racket/racket@1bfb999, `make -j n --disable-constant`
+;; silently discarded the `--disable-constant` flag, so we need this
+;; check to avoid parallel compilation on those versions of Racket.
+(define make-in-parallel?
+  (not (version<? (version) "8.0.2.4")))
+
 (define racket-exe (find-executable-path "racket"))
 (define raco-exe (find-executable-path "raco"))
 (define (raco . args)
   (apply system*/exit-code raco-exe args))
 
 (define make!-sema (make-semaphore 1))
-(define (make! path #:parallel? [parallel? #t])
+(define (make! path #:parallel? [parallel? make-in-parallel?])
   (call-with-semaphore make!-sema
     (lambda ()
       (zero?
