4ebcc2530a67aa0891c0d024ce3434fe8d10d3af
Jesse Alama <jesse@lisp.sh>
2021-11-15 06:47:54 +0100

    random: handle odd-length random strings

--- koyo-lib/koyo/random.rkt
+++ koyo-lib/koyo/random.rkt
@@ -2,7 +2,6 @@
 
 (require racket/contract
          racket/fixnum
-         racket/format
          racket/port
          racket/random
          racket/string)
@@ -12,12 +11,22 @@
  generate-random-string)
 
 (define (crypto-generate-random-string len)
-  (with-output-to-string
-    (lambda ()
-      (for ([b (in-bytes (crypto-random-bytes (/ len 2)))])
-        (when (fx< b 16)
-          (display "0"))
-        (display (number->string b 16))))))
+  (define num-bs
+    (quotient
+     (if (odd? len)
+         (add1 len)
+         len)
+     2))
+  (define str
+    (with-output-to-string
+      (lambda ()
+        (for ([b (in-bytes (crypto-random-bytes num-bs))])
+          (when (fx< b 16)
+            (display "0"))
+          (display (number->string b 16))))))
+  (if (odd? len)
+      (substring str 0 len)
+      str))
 
 (define/contract current-random-string-generator
   (parameter/c (-> exact-positive-integer? non-empty-string?))
