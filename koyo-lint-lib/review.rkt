f87c4fd8ab5c85b07acf8405d2bde759669a6572
Bogdan Popa <bogdan@defn.io>
2024-12-22 18:10:10 +0200

    lint: add lints for with-database-{connection,transaction}

--- koyo-lint-lib/review.rkt
+++ koyo-lint-lib/review.rkt
@@ -10,9 +10,27 @@
  review-syntax)
 
 (define (should-review-syntax? stx)
-  (syntax-case stx (define-option)
+  (syntax-case stx (define-option with-database-connection with-database-transaction)
     [(define-option . _rest) #t]
+    [(with-database-connection . _rest) #t]
+    [(with-database-transaction . _rest) #t]
     [_ #f]))
+
+(define-syntax-class database-block
+  #:datum-literals (with-database-connection with-database-transaction)
+  (pattern (with-database-connection
+             {~do (push-scope)}
+             [conn-id:id db:expression]
+             {~do (track-binding #'conn-id "~a")}
+             body:expression ...+
+             {~do (pop-scope)}))
+  (pattern (with-database-transaction
+             {~do (push-scope)}
+             [conn-id:id db:expression]
+             {~do (track-binding #'conn-id "~a")}
+             {~optional {~seq #:isolation isolation:expression}}
+             body:expression ...+
+             {~do (pop-scope)})))
 
 (define-syntax-class option-definition
   #:datum-literals (define-option)
@@ -26,4 +44,5 @@
 (define (review-syntax stx)
   (syntax-parse stx
     [c:option-definition #'c]
+    [d:database-block #'d]
     [_ (track-error stx "expected a option definition")]))
